4ad351bcaa7c9bcf1ecd4b67aa84b033b28fcc06
tsMurari <119278443+tsMurari@users.noreply.github.com>
2023-01-13 20:52:55 +0530

    Add files via upload
    This code creates an object called person that has properties for the person's name, age, and job. It then logs a message to the console that uses the object's properties to construct a sentence describing the person. The code then modifies the job property of the person object and adding a new property hobby and then logs a new message that again uses the properties of the object to construct a sentence, this time describing the person's updated job and hobby.

---
+++ day4code.js
@@ -0,0 +1,13 @@
+let person = {
+    name: "John",
+    age: 30,
+    job: "Developer",
+  };
+  
+  console.log(person.name + " is a " + person.age + " years old " + person.job + ".");
+  
+  person.job = "Manager";
+  person.hobby = "Music";
+  
+  console.log(person.name + " is now a " + person.job + " and his hobby is " + person.hobby + ".");
+  
