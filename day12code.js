d47e14710d57d25432912f57b31851a0528ef41b
tsMurari <119278443+tsMurari@users.noreply.github.com>
2023-01-21 20:16:18 +0530

    Update day12code.js

--- day12code.js
+++ day12code.js
@@ -1,16 +1,11 @@
 function smallestCommonMultiple() {
     let num1 = parseInt(prompt("Please enter the first number:"));
     let num2 = parseInt(prompt("Please enter the second number:"));
-    let multiple = 0;
-    let i = 1;
-    while (multiple === 0) {
-        let multiple1 = num1 * i;
-        let multiple2 = num2 * i;
-        if (multiple1 === multiple2) {
-            multiple = multiple1;
-        }
-        i++;
+    let multiple = 1;
+    while(!(multiple % num1 === 0 && multiple % num2 === 0)){
+        multiple++;
     }
     return multiple;
 }
 console.log("The smallest common multiple is: " + smallestCommonMultiple());
+
